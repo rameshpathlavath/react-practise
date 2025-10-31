29aef2d78bcafc094cbce4c5812dbcbb718d0c74
rameshpathlavath <rameshpathlavath795@gmail.com>
2025-10-31 11:07:02 +0530

    Initial commit

---
+++ Controllers/ERegisterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EX276_R.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ERegisterController : ControllerBase
+    {
+    }
+}
